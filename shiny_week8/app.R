6c1318ab355822fb5c7839634d727e1d28c10eb2
Vivien Lee <vivienlvv@gmail.com>
2023-03-17 16:45:31 -0500

    Setting up shiny directory

---
+++ shiny_week8/app.R
@@ -0,0 +1,20 @@
+library(shiny)
+
+
+# Defining user interface
+ui = fluidPage(
+  
+  
+)
+
+
+# Defining erver function
+server = function(input, output){
+  
+}
+
+
+shinyApp(ui = ui , server = server)
+
+# Deploying app at the end 
+# rsconnect::deployApp('shiny_week8/')
